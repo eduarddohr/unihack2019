0f157e90f5a728fdcc4faf466d0157bd7509c6b2
eduarddohr <eduarddohr@gmail.com>
2019-05-18 01:58:51 +0300

    added api for bins

---
+++ unihackAPI/unihackAPI/unihackAPI/Controllers/BinController.cs
@@ -0,0 +1,74 @@
+﻿using ResoApi;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using unihackAPI.Models;
+
+namespace unihackAPI.Controllers
+{
+
+    [Authorize]
+    [RoutePrefix("api/Bins")]
+    public class BinController : ApiController
+    {
+        [Route("GetBin")]
+        public async Task<IHttpActionResult> GetBin(Guid Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            DataTable dt = Utils.ExecuteTable(SQLCommands.GetBin(Id));
+            
+            BinModel bin = Utils.DataTableToList<BinModel>(dt)[0];
+            
+            if (dt.Rows.Count == 0)
+                return NotFound();
+            else
+                return Ok(bin);
+        }
+        [Route("GetBins")]
+        public async Task<IHttpActionResult> GetBins()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            DataTable dt = Utils.ExecuteTable(SQLCommands.GetBins());
+
+            List<BinModel> bin = Utils.DataTableToList<BinModel>(dt);
+            foreach(BinModel b in bin)
+            {
+                DataTable dtc = Utils.ExecuteTable(SQLCommands.GetCollectorsByBin(b.Id));
+                b.Collectors = Utils.DataTableToList<CollectorModel>(dtc);
+            }
+
+            if (dt.Rows.Count == 0)
+                return NotFound();
+            else
+                return Ok(bin);
+        }
+        [Route("AddBin")]
+        public async Task<IHttpActionResult> AddBin(BinModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            model.Id = Guid.NewGuid();
+            Utils.ExecuteNonQuery(SQLCommands.AddBin(model));
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ unihackAPI/unihackAPI/unihackAPI/SQLCommands.cs
@@ -0,0 +1,83 @@
+﻿using unihackAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ResoApi
+{
+    public class SQLCommands
+    {
+        public static SqlCommand GetUsers()
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            string str = "select * from [dbo].[AspNetUsers]";
+            cmd.CommandText = str;
+
+            return cmd;        
+        }
+        #region Bin
+        public static SqlCommand GetBins()
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            string str = @"select B.Id, B.Capacity, B.Latitude, B.Longitude, B.Name, B.Type, B.Zone, MA.ManagerId, U.Name as ManagerName from [dbo].[Bins] B
+                            inner join [dbo].[ManagerArea] MA on B.Zone = MA.Zone
+                            inner join [dbo].[AspNetUsers] U on MA.ManagerId = U.Id";
+            cmd.CommandText = str;
+
+            return cmd;
+        }
+        public static SqlCommand GetBin(Guid Id)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            string str = "select * from [dbo].[Bins] where Id = @ID";
+            cmd.CommandText = str;
+            cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = Id;
+
+            return cmd;
+        }
+        public static SqlCommand AddBin(BinModel model)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            string str = "insert into [dbo].[Bins] (Id, Name, Type, Latitude, Longitude, Capacity, Zone) values (@mId, @mName, @mType, @mLatitude, @mLongitude, @mCapacity, @mZone)";
+            cmd.CommandText = str;
+            cmd.Parameters.Add("@mId", SqlDbType.UniqueIdentifier).Value = model.Id;
+            cmd.Parameters.Add("@mName", SqlDbType.NVarChar).Value = model.Name;
+            cmd.Parameters.Add("@mType", SqlDbType.Int).Value = model.Type;
+            cmd.Parameters.Add("@mLatitude", SqlDbType.Float).Value = model.Latitude;
+            cmd.Parameters.Add("@mLongitude", SqlDbType.Float).Value = model.Longitude;
+            cmd.Parameters.Add("@mCapacity", SqlDbType.Float).Value = model.Capacity;
+            cmd.Parameters.Add("@mZone", SqlDbType.Int).Value = model.Zone;
+
+            return cmd;
+        }
+        public static SqlCommand DeleteBin(Guid Id)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            string str = "delete from [dbo].[Bins] where Id = @ID";
+            cmd.CommandText = str;
+            cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = Id;
+
+            return cmd;
+        }
+        public static SqlCommand GetCollectorsByBin(Guid Id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            string str = @"SELECT BUA.[UserId] as Id, U.Name as Name FROM[dbo].[BinUserAssociation] BUA
+                             inner join[dbo].[AspNetUsers] U on BUA.UserId = U.Id where BinId = @mID";
+            cmd.CommandText = str;
+            cmd.Parameters.Add("@mId", SqlDbType.UniqueIdentifier).Value = Id;
+
+            return cmd;
+        }
+        #endregion Bin
+
+    }
+}
