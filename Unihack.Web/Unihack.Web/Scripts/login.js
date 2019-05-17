3c10e6d160880e871c9c74dcb4a4baafc876e6de
marcelam28 <motiu_m@yahoo.com>
2019-05-17 19:48:24 +0300

    added register and login

---
+++ Unihack.Web/Unihack.Web/Scripts/login.js
@@ -0,0 +1,33 @@
+﻿function login() {
+	debugger;
+	EasyLoading.show();
+	var email = $('#email').val();
+	var password = $('#pass').val();
+	var formData = {
+		"Email": email,
+		"Password": password,
+	};
+	if (email != "" && password != "") {
+		AjaxHelper.post(
+			'Account/Login',
+			formData,
+			function (response) {
+				if (response == 1) {
+					EasyLoading.hide();
+					window.location = "http://localhost:63063/Home/Index"
+					$('.#email').val("");
+					$('#pass').val("");
+				}
+				else{
+					EasyLoading.hide();
+					toastr.error("An error occured");
+				}
+			},
+			function () {
+				EasyLoading.hide();
+				toastr.error("An error occured");
+			}
+		)
+	}
+}
+
---
+++ Unihack.Web/Unihack.Web/Scripts/register.js
@@ -0,0 +1,37 @@
+﻿function register() {
+	debugger;
+	EasyLoading.show();
+	var email = $('#r-email').val();
+	var password = $('#r-pass').val();
+	var password2 = $('#r-pass2').val();
+	var formData = {
+		"Email": email,
+		"Password": password,
+		"ConfirmPassword": password2
+	};
+	if (email != "" && password != "" && password2 != "") {
+		debugger
+		AjaxHelper.post(
+			'Account/Register',
+			formData,
+			function (response) {
+				debugger
+				EasyLoading.hide();
+				if (response == 1) {
+					window.location = "http://localhost:63063/Account/Login";
+					$('.#r-email').val("");
+					$('#r-pass').val("");
+					$('#r-pass2').val("");
+				}
+				if (response == -1) {
+					$('#r-email').val("");
+					$('#r-pass').val("");
+					$('#r-pass2').val("");
+				}
+			},
+			function () {
+				EasyLoading.hide();
+			}
+		)
+	}
+}
