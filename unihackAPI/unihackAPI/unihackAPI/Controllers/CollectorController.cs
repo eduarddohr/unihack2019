96b53be68a4ad326cf390ecb885eb8ae75e04aba
eduarddohr <eduarddohr@gmail.com>
2019-05-18 22:53:17 +0300

    added controller for collectors

---
+++ unihackAPI/unihackAPI/unihackAPI/Controllers/CollectorController.cs
@@ -0,0 +1,36 @@
+﻿using ResoApi;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using unihackAPI.Models;
+
+namespace unihackAPI.Controllers
+{
+    [RoutePrefix("api/Collector")]
+    public class CollectorController : ApiController
+    {
+        [Route("GetCollectorsByManager")]
+        public async Task<IHttpActionResult> GetCollectorsByManager(string Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            DataTable dt = Utils.ExecuteTable(SQLCommands.GetCollectorsByManager(Id));
+
+            List<CollectorModel> collectors = Utils.DataTableToList<CollectorModel>(dt);
+           
+
+            if (dt.Rows.Count == 0)
+                return NotFound();
+            else
+                return Ok(collectors);
+        }
+    }
+}
